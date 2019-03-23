817097312d4d0a3409d03841085b007e6de968ce
Ethan Mick <ethan@ethanmick.com>
2019-03-23 18:05:29 -0400

    Now we have some sweet tailwinds

--- components/Chapter.tsx
+++ components/Chapter.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export const Chapter = ({ children, meta }) => (
   <article>
-    <h1>{meta.title}</h1>
+    <h1 className="text-purple leading-normal">{meta.title}</h1>
     {children}
   </article>
 )
--- next.config.js
+++ next.config.js
@@ -1,12 +1,14 @@
 const withTypescript = require('@zeit/next-typescript')
-
+const withCSS = require('@zeit/next-css')
 const withMDX = require('@zeit/next-mdx')({
   extension: /\.mdx?$/,
   options: {}
 })
 
-module.exports = withTypescript(
-  withMDX({
-    pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx']
-  })
+module.exports = withCSS(
+  withTypescript(
+    withMDX({
+      pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx']
+    })
+  )
 )
